cb4c014803c62035cbe9d9cb91d10cd0487e1238
sakaguchi <sgucci.0522@gmail.com>
2026-02-15 08:56:57 +0000

    streamlit うまく動作する

---
+++ ui/chat.py
@@ -0,0 +1,46 @@
+import sys
+from pathlib import Path
+
+# 現在のファイルの場所を絶対パスに変換　.parent 一つ上のフォルダ
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+import streamlit as st
+from app.main import app  # あなたのRAGチェーン
+from app.main import run_rag
+
+st.title("📄 RAG Chat System")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# チャット履歴表示　session_state ブラウザセッション単位の保存領域
+# 過去の会話を１件ずつ取り出す
+for msg in st.session_state.messages:
+
+# roleに応じて　"user"-> ユーザー　"assistant"->ＡＩ
+    with st.chat_message(msg["role"]):
+
+# メッセージ内容を表示
+        st.markdown(msg["content"])
+
+# 入力
+if prompt := st.chat_input("質問を入力してください"):
+
+# ユーザーの入力内容を履歴に追加、吹き出しを表示
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # RAG実行
+    result = app.invoke({"question": prompt})
+
+# RAGの戻り値から回答部分だけ取り出す
+#   answer = result["answer"]
+    answer = run_rag(prompt)
+
+# ＡＩの返答を履歴に保存、吹き出しを表示
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+
+    with st.chat_message("assistant"):
+        st.markdown(answer)
